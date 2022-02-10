c235f70d30155d568321f8f6f0e26993d159f956
Basili Artomo <artamoshinbasil@yandex.ru>
2022-02-11 02:31:53 +0300

    scc bad work,js i dont check

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// use App\Http\Controllers\MainController;
+Route::get('/', 'MainController@index');
